50cf527c6d69a268880988748afd4ed4fc7ae75f
Jacek Krajewski <jacekkrajewski12@wp.pl>
2025-02-26 18:49:07 +0100

    remove language support

--- src/utils/common/urls.ts
+++ src/utils/common/urls.ts
@@ -1,6 +1,4 @@
-import { Countries } from '@/utils/common/countries';
-
 export enum PATHS {
-  HOME = `/${Countries.EN}`,
-  ARTICLES = `/${Countries.EN}/articles`,
+  HOME = `/`,
+  ARTICLES = `/articles`,
 }
