6702840da20e03d62b5dbda0f30658e1842d4fb9
Jacek Krajewski <jacekkrajewski12@wp.pl>
2025-03-12 16:57:47 +0100

    add footer links

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -1,9 +1,31 @@
+import { Link } from '@/components/common/link/Link';
 import styles from './footer.module.css';
+import { ArticleNames, ArticleURLS } from '@/utils/articleData/articles';
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>&copy; 2025 LinguoLog</p>
+      <div className={styles.footerLinksSection}>
+        <div className={styles.footerLinkGroup}>
+          <p className={styles.footerLinkGroupHeader}>Useful language tools</p>
+          <a className={styles.footerLinkItem} href="https://merienda.pl/">
+            Merienda.pl – Learn Spanish in just 5 minutes a day
+          </a>
+        </div>
+        <div className={styles.footerLinkGroup}>
+          <p className={styles.footerLinkGroupHeader}>Trending articles</p>
+          <Link className={styles.footerLinkItem} href={ArticleURLS.BRITISH_ACCENT}>
+            {ArticleNames.BRITISH_ACCENT}
+          </Link>
+          <Link className={styles.footerLinkItem} href={ArticleURLS.MISTAKES_IN_LEARNING}>
+            {ArticleNames.MISTAKES_IN_LEARNING}
+          </Link>
+          <Link className={styles.footerLinkItem} href={ArticleURLS.IMPROVE_SPELLING}>
+            {ArticleNames.IMPROVE_SPELLING}
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 };
